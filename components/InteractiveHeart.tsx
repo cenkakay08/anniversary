c19600b6f0805991dcbc4d21499234bd412598d3
cenkakay08 <cenk_akay08@hotmail.com>
2026-01-13 22:03:38 +0300

    style: Use aspect-ratio for lovers container sizing

--- components/InteractiveHeart.tsx
+++ components/InteractiveHeart.tsx
@@ -45,7 +45,7 @@ InteractiveHeartContent.displayName = "InteractiveHeartContent";
 const InteractiveHeart = memo(() => {
   return (
     <div className="flex shrink-0 flex-col items-center gap-2">
-      <div className="relative flex h-[180px] w-[180px] flex-col items-center justify-center sm:size-60">
+      <div className="relative flex aspect-[15/8] w-[180px] flex-col items-center justify-center sm:w-60">
         <SpringProvider>
           <InteractiveHeartContent />
         </SpringProvider>
