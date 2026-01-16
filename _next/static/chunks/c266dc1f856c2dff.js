25ee0cf86c9d53d51c014bdef94485f2cfe9be1c
cenkakay08 <cenkakay08@users.noreply.github.com>
2026-01-16 15:29:55 +0000

    Deploying to gh-pages from @ cenkakay08/anniversary@bf83f65ee8da4fdfd28203a648a8f5de2ffeb9a5 🚀

---
+++ _next/static/chunks/c266dc1f856c2dff.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/d0d93a23370c77aa.js",
+  "static/chunks/c92d4a270abb0d7a.js",
+  "static/chunks/f2aa92c526591ea8.js",
+  "static/chunks/7b338cc9221585c0.css",
+  "static/chunks/turbopack-008dc30b94fd1537.js"
+])
--- _next/static/chunks/f919d1281432c7af.js
+++
@@ -1,7 +0,0 @@
-__turbopack_load_page_chunks__("/", [
-  "static/chunks/d0d93a23370c77aa.js",
-  "static/chunks/a2757474603cbb1a.js",
-  "static/chunks/f2aa92c526591ea8.js",
-  "static/chunks/7b338cc9221585c0.css",
-  "static/chunks/turbopack-5b895201e80cb80c.js"
-])
