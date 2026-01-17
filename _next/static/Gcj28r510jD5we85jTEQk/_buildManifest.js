59c71e866f01dbecfb3c6d3db8093bc16e25d5a3
cenkakay08 <cenkakay08@users.noreply.github.com>
2026-01-17 19:49:08 +0000

    Deploying to gh-pages from @ cenkakay08/anniversary@779cc85171563a5ee38edc397662e93ceaec6592 🚀

---
+++ _next/static/Gcj28r510jD5we85jTEQk/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/2eab8654f67b3868.js"
+  ],
+  "/_error": [
+    "static/chunks/c46230e592c81271.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/2eab8654f67b3868.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/c7a5e9c2da620ef9.js",
+  "static/chunks/fac18a28dfde3845.js",
+  "static/chunks/f2aa92c526591ea8.js",
+  "static/chunks/7b338cc9221585c0.css",
+  "static/chunks/turbopack-fe4f89a2eb961568.js"
+])
