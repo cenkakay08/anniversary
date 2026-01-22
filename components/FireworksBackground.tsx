013d63b5fa3f98c05181f33dfd0d810fb377d444
cenkakay08 <cenk_akay08@hotmail.com>
2026-01-22 16:50:34 +0300

    feat: add explosion sounds to fireworks background

--- components/FireworksBackground.tsx
+++ components/FireworksBackground.tsx
@@ -1,19 +1,33 @@
 import { Fireworks } from "@fireworks-js/react";
 import type { FireworksHandlers } from "@fireworks-js/react";
-import React, { memo, useRef } from "react";
+import { memo, useRef } from "react";
 
 const FireworksBackground = memo(() => {
-    const ref = useRef<FireworksHandlers>(null);
+  const ref = useRef<FireworksHandlers>(null);
 
-    return (
-        <Fireworks
-            ref={ref}
-            className="absolute top-0 left-0 size-full"
-
-        />
-    );
+  return (
+    <Fireworks
+      ref={ref}
+      className="absolute top-0 left-0 size-full"
+      options={{
+        intensity: 15,
+        sound: {
+          enabled: true,
+          files: [
+            "/sounds/website_public_sounds_explosion0.mp3",
+            "/sounds/website_public_sounds_explosion1.mp3",
+            "/sounds/website_public_sounds_explosion2.mp3",
+          ],
+          volume: {
+            min: 2,
+            max: 4,
+          },
+        },
+      }}
+    />
+  );
 });
 
-FireworksBackground.displayName = 'FireworksBackground';
+FireworksBackground.displayName = "FireworksBackground";
 
 export default FireworksBackground;
